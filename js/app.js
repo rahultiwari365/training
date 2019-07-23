5f3bbbabdecc86e196abe4aa95323315a58769ac
Rahul Tiwari <53038075+rahultiwari365@users.noreply.github.com>
2019-07-23 21:48:15 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
-var a=100;
+var a=[14,15,16,17,18,19];
 var sum=0;
-for(var i=0;i<a;i++)
-    sum=sum+i;
+for(var i=0;i<a.length;i++)
+    sum=sum+a[i];
 
 console.log("sum of 100 numbers is = " + sum);
