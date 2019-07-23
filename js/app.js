6a8fb6d485ef88b6a36594ef80053f61694d99fc
rahultiwari365 <rahul.tiwari@triconinfotech.com>
2019-07-23 21:30:50 +0530

    Version 1

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var a=100;
+var sum=0;
+for(var i=0;i<a;i++)
+    sum=sum+i;
+
+console.log("sum of 100 numbers is = " + sum);
---
+++ js/calculator.js
@@ -0,0 +1,25 @@
+var a=100;
+var b=200;
+var value=null;
+
+function sum(a,b){
+    var s;
+     s=a+b;
+    return s;
+}
+function minus(a,b){
+    var m=a-b;
+    return m;
+}
+function multi(a,b){
+    var mu=a*b;
+    return mu;
+}
+function div(a,b){
+    var d=a/b;
+    return d;
+}
+console.log(sum(a,b));
+console.log(minus(a,b));
+console.log(multi(a,b));
+console.log(div(a,b));
